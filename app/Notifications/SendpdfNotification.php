147f8a2ed5b688d244202020df605594b2a1be2f
Karima Akter <genie.karima@gmail.com>
2023-08-17 17:56:14 +0600

    make pdf attach fiile sending in mail

---
+++ app/Notifications/SendpdfNotification.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SendpdfNotification extends Notification
+{
+    use Queueable;
+    public $pdfPath;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($pdfPath)
+    {
+        $this->pdfPath = $pdfPath;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+
+    {
+        return (new MailMessage)
+                     ->subject('Employee Report')
+                     ->line('Here is the employee report you requested.')
+                    ->line('Please download the PDF.')
+                    ->attach($this->pdfPath, [
+                        'as' => 'document.pdf',
+                        'mime' => 'application/pdf',
+                    ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
