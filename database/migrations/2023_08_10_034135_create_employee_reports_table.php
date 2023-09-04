7065750510624e9f59114af6dbcb459285e1d110
Karima Akter <genie.karima@gmail.com>
2023-09-04 17:12:05 +0600

    cascade task

--- database/migrations/2023_08_10_034135_create_employee_reports_table.php
+++ database/migrations/2023_08_10_034135_create_employee_reports_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('employee_reports', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('employee_id');
+            $table->foreignId('employee_id')->references('id')->on('users')->onDelete('cascade');
             $table->date('date')->nullable();
             $table->time('check_in');
             $table->time('check_out')->nullable();
